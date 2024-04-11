0f2059a621e289e1740e9cc2bf4540c423ca2f99
James McManus <james.mcmanus@kellogg.northwestern.edu>
2024-04-11 16:04:49 +0000

    Went through and completed airbnb!

--- practice-6.sql
+++ practice-6.sql
@@ -15,4 +15,8 @@
 -- | Brighton Park          | 678      |
 -- | Burnside               | 10       |
 
-
+SELECT listings.neighborhood, COUNT(reviews.listing_id)
+FROM listings INNER JOIN reviews on listings.id = reviews.listing_id
+GROUP BY listings.neighborhood
+ORDER BY listings.neighborhood
+;
